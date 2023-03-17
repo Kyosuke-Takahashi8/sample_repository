a1901d8a0c4a53d4f0360d46956f0612a0f79e76
Kyosuke-Takahashi8 <ec2-user@ip-172-31-1-148.ec2.internal>
2023-03-17 19:38:59 +0900

    curriculum08-3

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [Postcontroller::class, 'show']);
