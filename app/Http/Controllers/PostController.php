9745c9d0a604d45fa8ccc49d1f0269814fe083a0
Kyosuke-Takahashi8 <ec2-user@ip-172-31-1-148.ec2.internal>
2023-03-23 21:38:00 +0900

    add1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     
     public function show(Post $post)
     {
-        return view('posts/show')->with(['post' => $post]);
+        
     }
     
     public function create()
